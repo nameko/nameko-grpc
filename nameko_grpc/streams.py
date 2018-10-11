f6990454493d2a18862437b55488cebbeaec5ca3
Matt Bennett <matt@bennett.name>
2018-10-11 15:53:17 +0100

    receive stream using bytebuffer

--- nameko_grpc/streams.py
+++ nameko_grpc/streams.py
@@ -2,52 +2,15 @@ from eventlet.queue import Queue, Empty
 import struct
 
 
-class ReceiveStream:
-    def __init__(self, stream_id, message_type):
-        self.stream_id = stream_id
-        self.message_type = message_type
-
-        self.message_queue = Queue()
-        self.buffer = bytearray()  # TODO use ByteBuffer
-
-    def close(self):
-        self.message_queue.put(None)
-
-    def write(self, data):
-
-        self.buffer.extend(data)
-
-        if len(self.buffer) < 5:
-            return
-
-        compressed_flag = struct.unpack("?", self.buffer[0:1])[0]
-        message_length = struct.unpack(">I", self.buffer[1:5])[0]
-
-        # TODO handle compression
-        if compressed_flag:
-            raise NotImplementedError
-
-        if len(self.buffer) < 5 + message_length:
-            return
-
-        limit = 5 + message_length
-        message = self.message_type()
-        message.ParseFromString(bytes(self.buffer[5:limit]))
-        self.buffer = self.buffer[limit:]  # or reset?
-
-        self.message_queue.put(message)
-
-    def messages(self):
-        while True:
-            message = self.message_queue.get()
-            if message is None:
-                break
-            yield message
+HEADER_LENGTH = 5
 
 
 class ByteBuffer:
     def __init__(self):
         self.bytes = bytearray()
+
+    def peek(self, slice):
+        return self.bytes[slice]
 
     def read(self, max_bytes):
         length = min(max_bytes, len(self.bytes))
@@ -63,6 +26,49 @@ class ByteBuffer:
 
     def __len__(self):
         return len(self.bytes)
+
+
+class ReceiveStream:
+    def __init__(self, stream_id, message_type):
+        self.stream_id = stream_id
+        self.message_type = message_type
+
+        self.message_queue = Queue()
+        self.buffer = ByteBuffer()
+
+    def close(self):
+        self.message_queue.put(None)
+
+    def write(self, data):
+
+        self.buffer.write(data)
+
+        if len(self.buffer) < HEADER_LENGTH:
+            return
+
+        compressed_flag = struct.unpack("?", self.buffer.peek(slice(0, 1)))[0]
+        message_length = struct.unpack(">I", self.buffer.peek(slice(1, 5)))[0]
+
+        # TODO handle compression
+        if compressed_flag:
+            raise NotImplementedError
+
+        if len(self.buffer) < HEADER_LENGTH + message_length:
+            return
+
+        _ = self.buffer.read(HEADER_LENGTH)
+
+        message = self.message_type()
+        message.ParseFromString(bytes(self.buffer.read(message_length)))
+
+        self.message_queue.put(message)
+
+    def messages(self):
+        while True:
+            message = self.message_queue.get()
+            if message is None:
+                break
+            yield message
 
 
 class SendStream:
