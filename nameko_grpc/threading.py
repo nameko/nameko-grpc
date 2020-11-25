ea151572d96922898616ebeb08faa8a7f3379bfc
Matt Yule-Bennett <matt@bennett.name>
2020-11-25 22:07:24 +0000

    target_with_callback should raise if target raises

--- nameko_grpc/threading.py
+++ nameko_grpc/threading.py
@@ -3,7 +3,9 @@ import sys
 
 
 def target_with_callback(target, args=(), kwargs=None, callback=None):
-
+    """ Executes `target(*args, **kwargs)` and optionally calls a `callback`
+    with the result or any exception raised.
+    """
     if kwargs is None:
         kwargs = {}
 
@@ -13,10 +15,11 @@ def target_with_callback(target, args=(), kwargs=None, callback=None):
         except Exception:
             res = None
             exc_info = sys.exc_info()
-            # print traceback?
+            raise
         else:
             exc_info = None
-        if callback:
-            callback(res, exc_info)
+        finally:
+            if callback:
+                callback(res, exc_info)
 
     return execute
