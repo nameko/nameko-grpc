3913f02526ab9aa7e874c5dea468ca1916dcbc37
Matt Yule-Bennett <matt@bennett.name>
2021-01-14 12:23:53 +0000

    drop py3.5

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Internet",
