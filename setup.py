bd2aa6e3bc28048659360edaecc5ac6160e032ba
Stephen Collinson <stephen@pacerevenue.com>
2023-09-21 15:32:28 +0100

    pin for earlier py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "grpcio",
         "protobuf",
         "googleapis-common-protos",
-        "importlib-metadata",
+        "importlib-metadata<5 ; python_version<='3.7'"
     ],
     extras_require={
         "dev": [
